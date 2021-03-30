b0652589e1f09b416dad8330ea935537ea213190
Mihail Ananyev <mihail.ananev123@gmail.com>
2021-03-30 17:05:40 +0300

    Обновил скрипты создания схемы БД и её инициализации

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO organization (name, full_name, inn, kpp, address,phone)
 VALUES ('Рога и копыта', 'ООО "Рога и копыта"', '1122334455', '111222333', 'г. Москва, ул. Ленина, д. 1', '+71234567890');
 
 INSERT INTO organization (name, full_name, inn, kpp, address,phone)
-VALUES ('Парикмахерская "Светлана"', 'ООО Парикмахерская "Светлана"', '2233445566', '666777888', 'г. Владимир, ул. Пушкина, д. 2', '+74564567809');
+VALUES ('Парикмахерская "Светлана"', 'ИП Светлана Сидорова', '223344556677', '666777888', 'г. Владимир, ул. Пушкина, д. 2', '+74564567809');
 
 INSERT INTO organization (name, full_name, inn, kpp, address,phone)
 VALUES ('ТРАНССИБСТАЛЬМАШ', 'ЗАО "ТРАНССИБСТАЛЬМАШ"', '3344556677', '333444555', 'г. Челябинск, ул. Советская, д. 3', '+79874568725');
@@ -14,23 +14,37 @@ INSERT INTO country (code,name) VALUES (804, 'Украина');
 INSERT INTO country (code,name) VALUES (112, 'Республика Беларусь');
 INSERT INTO country (code,name) VALUES (398, 'Казахстан');
 
--- Документы
-INSERT INTO document (code, name) VALUES ('03', 'Свидетельство о рождении');
-INSERT INTO document (code, name) VALUES ('07', 'Военный билет');
-INSERT INTO document (code, name) VALUES ('08', 'Временное удостоверение, выданное взамен военного билета');
-INSERT INTO document (code, name) VALUES ('10', 'Паспорт иностранного гражданина');
-INSERT INTO document (code, name) VALUES ('11', 'Свидетельство о рассмотрении ходатайства о признании лица
+-- Типы документов
+INSERT INTO document_type (doc_code, doc_name) VALUES ('03', 'Свидетельство о рождении');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('07', 'Военный билет');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('08', 'Временное удостоверение, выданное взамен военного билета');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('10', 'Паспорт иностранного гражданина');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('11', 'Свидетельство о рассмотрении ходатайства о признании лица
 беженцем на территории Российской Федерации по существу');
-INSERT INTO document (code, name) VALUES ('12', 'Вид на жительство в Российской Федерации');
-INSERT INTO document (code, name) VALUES ('13', 'Удостоверение беженца');
-INSERT INTO document (code, name) VALUES ('15', 'Разрешение на временное проживание в Российской Федерации');
-INSERT INTO document (code, name) VALUES ('18', 'Свидетельство о предоставлении временного убежища на территории
+INSERT INTO document_type (doc_code, doc_name) VALUES ('12', 'Вид на жительство в Российской Федерации');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('13', 'Удостоверение беженца');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('15', 'Разрешение на временное проживание в Российской Федерации');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('18', 'Свидетельство о предоставлении временного убежища на территории
 Российской Федерации');
-INSERT INTO document (code, name) VALUES ('21', 'Паспорт гражданина Российской Федерации');
-INSERT INTO document (code, name) VALUES ('23', 'Свидетельство о рождении, выданное уполномоченным органом
+INSERT INTO document_type (doc_code, doc_name) VALUES ('21', 'Паспорт гражданина Российской Федерации');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('23', 'Свидетельство о рождении, выданное уполномоченным органом
 иностранного государства');
-INSERT INTO document (code, name) VALUES ('24', 'Удостоверение личности военнослужащего Российской Федерации');
-INSERT INTO document (code, name) VALUES ('91', 'Иные документы');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('24', 'Удостоверение личности военнослужащего Российской Федерации');
+INSERT INTO document_type (doc_code, doc_name) VALUES ('91', 'Иные документы');
+
+-- Документы
+INSERT INTO document (doc_code, doc_number, doc_date) VALUES
+(21, 1234567890, parseDateTime('20200101000000','yyyyMMddHHmmss'));
+INSERT INTO document (doc_code, doc_number, doc_date) VALUES
+(21, 1234567809, parseDateTime('20200303000000','yyyyMMddHHmmss'));
+INSERT INTO document (doc_code, doc_number, doc_date) VALUES
+(10, 123454321, parseDateTime('20190525000000','yyyyMMddHHmmss'));
+INSERT INTO document (doc_code, doc_number, doc_date) VALUES
+(13, 1234567895, parseDateTime('20180713000000','yyyyMMddHHmmss'));
+INSERT INTO document (doc_code, doc_number, doc_date) VALUES
+(21, 1234658791, parseDateTime('20160305000000','yyyyMMddHHmmss'));
+INSERT INTO document (doc_code, doc_number, doc_date) VALUES
+(10, 1132435465, parseDateTime('20190405000000','yyyyMMddHHmmss'));
 
 -- Офисы
 INSERT INTO office (name, address, phone, org_id) VALUES ('Главный офис', 'г. Москва, ул. Ленина, д. 1', '+71234567890', 1);
@@ -41,15 +55,15 @@ INSERT INTO office (name, address, phone, org_id) VALUES ('Офис #2', 'г. Ч
 INSERT INTO office (name, address, phone, org_id) VALUES ('Склад', 'г. Челябинск, ул. Советская, д. 12', '+79874568784', 3);
 
 -- Пользователи
-INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_name,doc_number,doc_date,citizenship_code)
-VALUES (1,'Иван', 'Кузнецов', 'Директор', '+73456789023','21','Паспорт гражданина Российской Федерации','1234567890',parseDateTime('20200101000000','yyyyMMddHHmmss'),643);
-INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_name,doc_number,doc_date,citizenship_code)
-VALUES (2,'Пётр', 'Смирнов', 'Зам. директора', '+73456789065','21','Паспорт гражданина Российской Федерации','1234567809',parseDateTime('20200303000000','yyyyMMddHHmmss'),643);
-INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_name,doc_number,doc_date,citizenship_code)
-VALUES (3,'Светлана', 'Сидорова', 'Парикмахер', '+74564567809','10','Паспорт иностранного гражданина','123454321',parseDateTime('20190525000000','yyyyMMddHHmmss'),112);
-INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_name,doc_number,doc_date,citizenship_code)
-VALUES (4,'Виктор', 'Белых', 'Директор', '+79874568725','13','Удостоверение беженца','1234567895',parseDateTime('20180713000000','yyyyMMddHHmmss'),804);
-INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_name,doc_number,doc_date,citizenship_code)
-VALUES (5,'Дмитрий', 'Пчелов', 'Зам. директора', '+73456787892','21','Паспорт гражданина Российской Федерации','1234658791',parseDateTime('20160305000000','yyyyMMddHHmmss'),643);
-INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_name,doc_number,doc_date,citizenship_code)
-VALUES (6,'Роман', 'Балашов', 'Управляющий складом', '+73456785673','10','Паспорт иностранного гражданина','1132435465',parseDateTime('20190405000000','yyyyMMddHHmmss'),398);
+INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_number,citizenship_code)
+VALUES (1,'Иван', 'Кузнецов', 'Директор', '+73456789023','21','1234567890',643);
+INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_number,citizenship_code)
+VALUES (2,'Пётр', 'Смирнов', 'Зам. директора', '+73456789065','21','1234567809',643);
+INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_number,citizenship_code)
+VALUES (3,'Светлана', 'Сидорова', 'Парикмахер', '+74564567809','10','123454321',112);
+INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_number,citizenship_code)
+VALUES (4,'Виктор', 'Белых', 'Директор', '+79874568725','13','1234567895',804);
+INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_number,citizenship_code)
+VALUES (5,'Дмитрий', 'Пчелов', 'Зам. директора', '+73456787892','21','1234658791',643);
+INSERT INTO user (office_id, first_name,last_name,position,phone,doc_code,doc_number,citizenship_code)
+VALUES (6,'Роман', 'Балашов', 'Управляющий складом', '+73456785673','10','1132435465',398);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,93 +1,111 @@
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS organization;
 CREATE TABLE organization (
-id INT(11) COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(255) NOT NULL COMMENT 'Короткое название организации',
-full_name VARCHAR(255) NOT NULL COMMENT 'Полное название организации',
-inn VARCHAR(255) NOT NULL COMMENT 'ИНН',
-kpp VARCHAR(255) NOT NULL COMMENT 'КПП',
-address VARCHAR(255) NOT NULL COMMENT 'Адрес организации',
-phone VARCHAR(255) DEFAULT NULL COMMENT 'Телефон организации',
-is_active TINYINT(1) DEFAULT 1 COMMENT 'Активна ли организация',
-version INT(11) DEFAULT 0 COMMENT 'Для оптимистичной блокировки',
-CONSTRAINT CHK_organization_is_active CHECK ((is_active = 0) OR (is_active = 1)),
-CONSTRAINT UX_organization_inn UNIQUE (inn),
-CONSTRAINT UX_organization_kpp UNIQUE (kpp),
-INDEX IX_organization_phone (phone),
-INDEX IX_organization_name (name),
-INDEX IX_organization_address (address),
-INDEX IX_organization_is_active (is_active)
+  id INT(11) COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL COMMENT 'Короткое название организации',
+  full_name VARCHAR(255) NOT NULL COMMENT 'Полное название организации',
+  inn VARCHAR(255) NOT NULL COMMENT 'ИНН',
+  kpp VARCHAR(255) NOT NULL COMMENT 'КПП',
+  address VARCHAR(255) NOT NULL COMMENT 'Адрес организации',
+  phone VARCHAR(255) DEFAULT NULL COMMENT 'Телефон организации',
+  is_active TINYINT(1) DEFAULT 1 COMMENT 'Активна ли организация',
+  version INT(11) DEFAULT 0 COMMENT 'Для оптимистичной блокировки',
+  CONSTRAINT CHK_organization_is_active CHECK (
+    (is_active = 0)
+    OR (is_active = 1)
+  ),
+  CONSTRAINT UX_organization_inn UNIQUE (inn),
+  CONSTRAINT UX_organization_kpp UNIQUE (kpp),
+  INDEX IX_organization_phone (phone),
+  INDEX IX_organization_name (name),
+  INDEX IX_organization_address (address),
+  INDEX IX_organization_is_active (is_active)
 );
 
 DROP TABLE IF EXISTS office;
 CREATE TABLE office (
-id INT(11) COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(255) NOT NULL COMMENT 'Имя офиса',
-address VARCHAR(255) NOT NULL  COMMENT 'Адрес офиса',
-phone VARCHAR(255)  DEFAULT NULL COMMENT 'Телефон офиса',
-org_id INT(11)  NOT NULL COMMENT 'Идентификатор организации',
-is_active TINYINT(1) DEFAUlT 1 COMMENT 'Активен ли офис',
-version INT(11)  DEFAULT 0 COMMENT 'Для оптимистичной блокировки',
-INDEX IX_office_name (name),
-INDEX IX_office_address (address),
-INDEX IX_office_phone (phone),
-CONSTRAINT CHK_office_is_active CHECK ((is_active = 0) OR (is_active = 1)),
-INDEX IX_office_is_active (is_active),
-INDEX FK_office_org_id_idx (org_id),
-CONSTRAINT FK_office_org_id FOREIGN KEY (org_id) REFERENCES organization (id)
-ON DELETE NO ACTION ON UPDATE NO ACTION
+  id INT(11) COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL COMMENT 'Имя офиса',
+  address VARCHAR(255) NOT NULL COMMENT 'Адрес офиса',
+  phone VARCHAR(255) DEFAULT NULL COMMENT 'Телефон офиса',
+  org_id INT(11) NOT NULL COMMENT 'Идентификатор организации',
+  is_active TINYINT(1) DEFAUlT 1 COMMENT 'Активен ли офис',
+  version INT(11) DEFAULT 0 COMMENT 'Для оптимистичной блокировки',
+  INDEX IX_office_name (name),
+  INDEX IX_office_address (address),
+  INDEX IX_office_phone (phone),
+  CONSTRAINT CHK_office_is_active CHECK (
+    (is_active = 0)
+    OR (is_active = 1)
+  ),
+  INDEX IX_office_is_active (is_active),
+  INDEX FK_office_org_id_idx (org_id),
+  CONSTRAINT FK_office_org_id FOREIGN KEY (org_id) REFERENCES organization (id)
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+DROP TABLE IF EXISTS document_type;
+CREATE TABLE document_type (
+  doc_code VARCHAR(255) COMMENT 'Код документа' PRIMARY KEY,
+  doc_name VARCHAR(255) COMMENT 'Название документа' NOT NULL,
+
+  UNIQUE INDEX UX_document_type_name (doc_name)
 );
 
 DROP TABLE IF EXISTS document;
 CREATE TABLE document (
-code VARCHAR(255) COMMENT 'Код документа' PRIMARY KEY,
-name VARCHAR(255) COMMENT 'Название документа' NOT NULL,
-UNIQUE INDEX UX_document_name (name)
+  doc_code VARCHAR(255) NOT NULL COMMENT 'Код документа',
+  doc_number VARCHAR(255) NOT NULL COMMENT 'Номер документа',
+  doc_date DATE COMMENT 'Дата' NOT NULL,
+
+  PRIMARY KEY (doc_code, doc_number),
+  INDEX IX_document_code (doc_code),
+  CONSTRAINT FK_document_code FOREIGN KEY (doc_code) REFERENCES document_type (doc_code)
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+  INDEX IX_document_date (doc_date)
 );
 
 DROP TABLE IF EXISTS country;
 CREATE TABLE country (
-code int(11) COMMENT 'Код страны' PRIMARY KEY,
-name VARCHAR(255) COMMENT 'Название страны' NOT NULL,
-UNIQUE INDEX UX_country_name (name)
+  code int(11) COMMENT 'Код страны' PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название страны' NOT NULL,
+  UNIQUE INDEX UX_country_name (name)
 );
 
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
-id INT(11) COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-office_id INT(11) NOT NULL COMMENT 'Идентификатор офиса',
-first_name VARCHAR(255) NOT NULL COMMENT 'Имя пользователя',
-last_name VARCHAR(255) DEFAULT NULL COMMENT 'Фамилия пользователя',
-middle_name VARCHAR(255) DEFAULT NULL COMMENT 'Второе личное имя пользователя',
-position VARCHAR(255) NOT NULL COMMENT 'Должность',
-phone VARCHAR(255) DEFAULT NULL COMMENT 'Телефон',
-doc_code VARCHAR(255) DEFAULT NULL COMMENT 'Код документа',
-doc_name VARCHAR(255) DEFAULT NULL COMMENT 'Название документа',
-doc_number VARCHAR(255) DEFAULT NULL COMMENT 'Номер документа',
-doc_date DATE DEFAULT NULL COMMENT 'Дата документа',
-citizenship_code INT(11) DEFAULT NULL COMMENT 'Код гражданства',
-is_identified TINYINT(1) DEFAULT 1 COMMENT 'Идентифицирован ли пользователь',
-version INT(11) DEFAULT 0 COMMENT 'Для оптимистичной блокировки',
-CONSTRAINT CHK_user_is_identified CHECK ((is_identified = 0) OR (is_identified = 1)),
-INDEX IX_user_is_identified (is_identified),
-INDEX FK_user_office_id_idx (office_id),
-CONSTRAINT FK_user_office_id FOREIGN KEY (office_id) REFERENCES office (id)
-ON DELETE NO ACTION ON UPDATE NO ACTION,
-INDEX FK_user_doc_code_idx (doc_code),
-CONSTRAINT FK_user_doc_code FOREIGN KEY (doc_code) REFERENCES document (code)
-ON DELETE NO ACTION ON UPDATE NO ACTION,
-INDEX FK_user_doc_name_idx (doc_name),
-CONSTRAINT FK_user_doc_name FOREIGN KEY (doc_name) REFERENCES document (name)
-ON DELETE NO ACTION ON UPDATE NO ACTION,
-INDEX FK_user_citizenship_code_idx (citizenship_code),
-CONSTRAINT FK_user_citizenship_code FOREIGN KEY (citizenship_code) REFERENCES country (code)
-ON DELETE NO ACTION ON UPDATE NO ACTION,
-INDEX IX_user_first_name (first_name),
-INDEX IX_user_last_name (last_name),
-INDEX IX_user_position (position),
-INDEX IX_user_phone (phone),
-INDEX IX_user_doc_number (doc_number)
+  id INT(11) COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  office_id INT(11) NOT NULL COMMENT 'Идентификатор офиса',
+  first_name VARCHAR(255) NOT NULL COMMENT 'Имя пользователя',
+  last_name VARCHAR(255) DEFAULT NULL COMMENT 'Фамилия пользователя',
+  middle_name VARCHAR(255) DEFAULT NULL COMMENT 'Второе личное имя пользователя',
+  position VARCHAR(255) NOT NULL COMMENT 'Должность',
+  phone VARCHAR(255) DEFAULT NULL COMMENT 'Телефон',
+  doc_code VARCHAR(255) DEFAULT NULL COMMENT 'Код документа',
+  doc_number VARCHAR(255) DEFAULT NULL COMMENT 'Номер документа',
+  citizenship_code INT(11) DEFAULT NULL COMMENT 'Код гражданства',
+  is_identified TINYINT(1) DEFAULT 1 COMMENT 'Идентифицирован ли пользователь',
+  version INT(11) DEFAULT 0 COMMENT 'Для оптимистичной блокировки',
+
+  CONSTRAINT CHK_user_is_identified CHECK (
+    (is_identified = 0)
+    OR (is_identified = 1)
+  ),
+  INDEX IX_user_is_identified (is_identified),
+  INDEX FK_user_office_id_idx (office_id),
+  CONSTRAINT FK_user_office_id FOREIGN KEY (office_id) REFERENCES office (id)
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+  INDEX FK_user_citizenship_code_idx (citizenship_code),
+  CONSTRAINT FK_user_citizenship_code FOREIGN KEY (citizenship_code) REFERENCES country (code)
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+  INDEX IX_user_doc_code_doc_number (doc_code, doc_number),
+  CONSTRAINT FK_user_doc_code_doc_number FOREIGN KEY (doc_code, doc_number) REFERENCES document (doc_code, doc_number)
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+  INDEX IX_user_first_name (first_name),
+  INDEX IX_user_last_name (last_name),
+  INDEX IX_user_position (position),
+  INDEX IX_user_phone (phone)
 );
 
-SET FOREIGN_KEY_CHECKS=1;
+SET FOREIGN_KEY_CHECKS = 1;
